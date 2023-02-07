1e3c8c544fe21a4c2df57ac06f1fa2783dd4e183
DigiDuncan <digiduncan@gmail.com>
2023-02-07 18:09:57 -0500

    actual code

--- textscreen/objects/screen.py
+++ textscreen/objects/screen.py
@@ -5,13 +5,22 @@ from arcade.sections import Section
 class TextScreen(Section):
     def __init__(self, left: int, bottom: int, width: int, height: int, **kwargs):
         super().__init__(left, bottom, width, height, name = "TextScreen",
-            accept_keyboard_keys = True, accept_mouse_events = True, local_mouse_coordinates = True, **kwargs)
+            accept_keyboard_keys = True, accept_mouse_events = True, local_mouse_coordinates = True,
+            prevent_dispatch_view = None, prevent_dispatch = None, **kwargs)
 
-        self.text_lines = []
+        window = arcade.get_window()
+
+        self.text_lines = ["This is a test!"]
         self.font_size = 16
 
         self.screen_width: int = 40
         self.screen_height: int = 20
+
+        self.calculate_screen()
+
+        self.text = arcade.text.Text("\n".join(self.text_lines[-self.screen_height:]), 0, window.height,
+            font_size = self.font_size, font_name = "FiraCode Nerd Font Mono",
+            anchor_x = "left", anchor_y = "top", multiline = True, width = window.width)
 
     def calculate_screen(self):
         test_character = arcade.text.Text("a", 0, 0,
@@ -23,3 +32,23 @@ class TextScreen(Section):
 
     def on_update(self, delta_time: float):
         return super().on_update(delta_time)
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        return super().on_key_press(symbol, modifiers)
+
+    def on_draw(self):
+        if self.view.filter_on:
+            # Draw our stuff into the CRT filter instead of on screen
+            self.view.crt_filter.use()
+            self.view.crt_filter.clear()
+            self.text.draw()
+
+            # Next, switch back to the screen and dump the contents of the CRT filter
+            # to it.
+            self.view.window.use()
+            self.view.clear()
+            self.view.crt_filter.draw()
+        else:
+            self.view.window.use()
+            self.view.clear()
+            self.text.draw()
--- textscreen/views/screen.py
+++ textscreen/views/screen.py
@@ -1,6 +1,11 @@
 import logging
+from turtle import Vec2D
 
 import arcade
+import arcade.key
+from arcade.experimental.crt_filter import CRTFilter
+
+from textscreen.objects.screen import TextScreen
 
 logger = logging.getLogger("textscreen")
 
@@ -9,11 +14,30 @@ class ScreenView(arcade.View):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        window = arcade.get_window()
+        self.section = TextScreen(0, 0, window.width, window.height)
+        self.section.enabled = True
+        self.section_manager.add_section(self.section)
+
+        self.crt_filter = CRTFilter(window.width, window.height,
+                            resolution_down_scale=1.0,
+                            hard_scan=-8.0,
+                            hard_pix=-3.0,
+                            display_warp = Vec2D(1.0 / 32.0, 1.0 / 18.0),
+                            mask_dark=0.5,
+                            mask_light=1.5)
+        self.filter_on = True
+
     def setup(self):
         pass
 
     def on_show_view(self):
         pass
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        print(f"wanted {arcade.key.F} and modifiers to be & 2, got {symbol} and {modifiers} ({modifiers & 2})")
+        if symbol == arcade.key.F and modifiers & arcade.key.MOD_CTRL:
+            self.filter_on = not self.filter_on
 
     def on_update(self, delta_time):
         pass
